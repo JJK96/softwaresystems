854ce1aeb7e809ec9f9ac2b34aa8c2fb153cca75
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2015-11-26 13:47:29 +0100

    added my name

--- test/src/test/Hello_git.java
+++ test/src/test/Hello_git.java
@@ -5,7 +5,7 @@ public class Hello_git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, Git!");
-		
+		System.out.println("Jan-Jaap");
 	}
 
 }
